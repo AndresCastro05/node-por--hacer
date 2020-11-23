1061c68c8d32878957147b5cd20f4c33da6cb818
andres <andrespte051216@gmail.com>
2020-11-23 16:03:52 -0500

    comit

---
+++ config/yargs.js
@@ -0,0 +1,35 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'descripcion de la tarea por hacer'
+
+};
+
+const completado = {
+    demand: true,
+    alias: 'c',
+    desc: 'marca como completado o pendiente la tarea'
+
+};
+
+
+const argv = require('yargs')
+    .command('crear', 'crear un elemento por hacer', {
+        descripcion
+    })
+
+.command('actualizar', 'actualizar el estado completo de una tarea', {
+    descripcion,
+    completado
+})
+
+.command('borrar', 'borra una tarea ', {
+        descripcion
+
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
